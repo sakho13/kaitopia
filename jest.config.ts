7c0ee7a1535d0d80023eca4b4638cbc16ec958f5
sakho13 <wecl.khotaro@gmail.com>
2025-04-12 11:03:29 +0900

    🔧 Jestの設定を更新し、テスト環境を明示的に指定。セットアップファイルを追加。

--- jest.config.ts
+++ jest.config.ts
@@ -7,8 +7,11 @@ const createJestConfig = nextJest({
 
 const config: Config = {
   coverageProvider: "v8",
-  testEnvironment: "jsdom",
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "@/(.*)$": "<rootDir>/src/$1",
+  },
 }
 
 export default createJestConfig(config)
