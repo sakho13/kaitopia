28660c8a92d20e5a42d374b3bc2d6ac6bf3c5f0d
sakho13 <wecl.khotaro@gmail.com>
2025-04-12 22:38:08 +0900

    ✨ ユーザー設定ページを新規作成し、基本的なレイアウトを実装

---
+++ src/app/v1/user/settings/page.tsx
@@ -0,0 +1,11 @@
+import { RoundedFrame } from "@/components/atoms/RoundedFrame"
+
+export default function Page() {
+  return (
+    <div className='bg-background min-h-screen text-text font-sans p-8'>
+      <RoundedFrame>
+        <h1>設定</h1>
+      </RoundedFrame>
+    </div>
+  )
+}
