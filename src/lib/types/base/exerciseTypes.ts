67fdc5dd4156d8e6a4c8d2883cfabda34b21d134
sakho13 <wecl.khotaro@gmail.com>
2025-04-13 20:15:21 +0900

    ✨ ExerciseおよびQuestionタイプの新規作成

---
+++ src/lib/types/base/exerciseTypes.ts
@@ -0,0 +1,24 @@
+export type Exercise = ExerciseBaseIdentifier &
+  ExerciseBase &
+  ExerciseBaseProperty &
+  ExerciseBaseDate
+
+export type ExerciseBaseIdentifier = {
+  exerciseId: string
+}
+
+export type ExerciseBase = {
+  title: string
+  description: string
+}
+
+export type ExerciseBaseProperty = {
+  schoolId: string
+  isCanSkip: boolean
+  isScoringBatch: boolean
+}
+
+export type ExerciseBaseDate = {
+  createdAt: string
+  updatedAt: string
+}
---
+++ src/lib/types/base/questionTypes.ts
@@ -0,0 +1,108 @@
+// *********************
+//       Question
+// *********************
+
+export type Question = QuestionBaseIdentifier & QuestionBase
+
+export type QuestionBaseIdentifier = {
+  questionId: string
+  schoolId: string
+}
+
+export type QuestionBase = {
+  title: string
+  currentVersion: number
+  questionType: QuestionTypeType
+  answerType: QuestionAnswerTypeType
+}
+
+export type QuestionBaseStatus = {
+  isPublished: boolean
+}
+
+export type QuestionBaseEditState = {
+  draftVersion: number
+}
+
+export type QuestionBaseAnswers = {
+  answers: QuestionAnswer[]
+}
+
+export type QuestionVersion = QuestionVersionBaseIdentifier &
+  QuestionVersionBase
+
+export type QuestionVersionBaseIdentifier = {
+  questionId: string
+  version: number
+}
+
+export type QuestionVersionBase = {
+  content: string
+  hint: string
+}
+
+// *********************
+//    QuestionAnswer
+// *********************
+
+export type QuestionAnswer = QuestionAnswerBaseIdentifier
+
+export type QuestionAnswerBaseIdentifier = {
+  questionId: string
+  version: number
+  answerId: string
+}
+
+export type QuestionAnswerProperty = {
+  SELECT: {
+    selection: {
+      answerId: string
+      selectContent: string
+    }[]
+  }
+  MULTI_SELECT: {
+    selection: {
+      answerId: string
+      selectContent: string
+    }[]
+  }
+  TEXT: {
+    property: {
+      answerId: string
+      minLength: number
+      maxLength: number
+    }
+  }
+}
+
+export type QuestionAnswerContent = {
+  SELECT: {
+    answerId: string
+  }
+  MULTI_SELECT: {
+    answerId: string[]
+  }
+  TEXT: {
+    content: string
+  }
+}
+
+export type UserAnswer<T extends QuestionAnswerTypeType> = {
+  type: T
+  answer: QuestionAnswerContent[T]
+}
+
+export const QuestionType = {
+  TEXT: "TEXT",
+  IMAGE: "IMAGE",
+} as const
+
+export type QuestionTypeType = keyof typeof QuestionType
+
+export const QuestionAnswerType = {
+  SELECT: "SELECT",
+  MULTI_SELECT: "MULTI_SELECT",
+  TEXT: "TEXT",
+} as const
+
+export type QuestionAnswerTypeType = keyof typeof QuestionAnswerType
