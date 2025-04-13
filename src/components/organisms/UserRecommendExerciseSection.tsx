7abf4bc3620c4d980688df38e2f70050b0966ab4
sakho13 <wecl.khotaro@gmail.com>
2025-04-13 17:20:30 +0900

    ✨ useGetRecommendExercisesフックを追加し、UserRecommendExerciseSectionコンポーネントを更新してAPIからのデータ取得を実装

--- src/components/organisms/UserRecommendExerciseSection.tsx
+++ src/components/organisms/UserRecommendExerciseSection.tsx
@@ -1,25 +1,14 @@
 "use client"
 
-import { useState } from "react"
 import { UserLayoutSection } from "../molecules/UserLayoutSection"
 import { encodeBase64 } from "@/lib/functions/encodeBase64"
 import { joincn } from "@/lib/functions/joincn"
 import { useRouter } from "next/navigation"
+import { useGetRecommendExercises } from "@/hooks/useApiV1"
 
 export function UserRecommendExerciseSection() {
   const router = useRouter()
-  const [recommendExercises, setRecommendExercises] = useState([
-    {
-      id: "intro_programming_1",
-      title: "プログラミング入門",
-      description: "入門者向けのプログラミング問題集",
-    },
-    {
-      id: "fundamental_information_technology_engineer_exam_1",
-      title: "基本情報技術者試験 問題集1",
-      description: "基本情報技術者試験の問題集",
-    },
-  ])
+  const { dataTooGetRecommendExercises } = useGetRecommendExercises()
 
   return (
     <UserLayoutSection
@@ -34,22 +23,28 @@ export function UserRecommendExerciseSection() {
       }
     >
       <div className='flex gap-x-3'>
-        {recommendExercises.map((exercise) => (
-          <div
-            key={encodeBase64(exercise.id)}
-            className={joincn(
-              `bg-background-subtle p-4 rounded-2xl shadow w-[300px]`,
-              `hover:shadow-lg transition`,
-              `cursor-pointer`,
-            )}
-            onClick={() =>
-              router.push(`/v1/user/exercise?eid=${encodeBase64(exercise.id)}`)
-            }
-          >
-            <h3 className='text-lg font-bold mb-2'>{exercise.title}</h3>
-            <p className='text-sm'>{exercise.description}</p>
-          </div>
-        ))}
+        {dataTooGetRecommendExercises?.success
+          ? dataTooGetRecommendExercises.data.recommendExercises.map(
+              (exercise) => (
+                <div
+                  key={encodeBase64(exercise.id)}
+                  className={joincn(
+                    `bg-background-subtle p-4 rounded-2xl shadow w-[300px]`,
+                    `hover:shadow-lg transition`,
+                    `cursor-pointer`,
+                  )}
+                  onClick={() =>
+                    router.push(
+                      `/v1/user/exercise?eid=${encodeBase64(exercise.id)}`,
+                    )
+                  }
+                >
+                  <h3 className='text-lg font-bold mb-2'>{exercise.title}</h3>
+                  <p className='text-sm'>{exercise.description}</p>
+                </div>
+              ),
+            )
+          : null}
       </div>
     </UserLayoutSection>
   )
---
+++ src/hooks/useApiV1.ts
@@ -0,0 +1,36 @@
+import useSwr from "swr"
+import { useAuth } from "./useAuth"
+import { ApiV1OutBase, ApiV1OutTypeMap } from "@/lib/types/apiV1Types"
+
+const fetcher = async <T extends keyof ApiV1OutTypeMap>(
+  _label: T,
+  method: string,
+  url: string,
+  token: string,
+) =>
+  fetch(url, {
+    method,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => {
+    if (!res.ok) return res.json()
+    return res.json()
+  }) as Promise<ApiV1OutBase<ApiV1OutTypeMap[T]>>
+
+export function useGetRecommendExercises() {
+  const { idToken } = useAuth()
+
+  const { data, isLoading } = useSwr(
+    ["/api/common/v1/recommend-exercise", idToken],
+    async ([url, token]) =>
+      token ? fetcher("GetRecommendExercise", "GET", url, token) : null,
+  )
+
+  return {
+    dataTooGetRecommendExercises: data,
+    isLoadingToGetRecommendExercises: isLoading,
+  } as const
+}
+
+export function useGetExercises() {}
