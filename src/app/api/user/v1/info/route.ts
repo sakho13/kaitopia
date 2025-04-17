563fc51f9ac27f4afd329321d2a022cf22c0aba9
sakho13 <wecl.khotaro@gmail.com>
2025-04-18 07:39:31 +0900

    🚚 ユーザ登録POSTエンドポイントを移動

--- src/app/api/user/v1/info/route.ts
+++ src/app/api/user/v1/info/route.ts
@@ -25,24 +25,3 @@ export async function GET(request: NextRequest) {
     }
   })
 }
-
-export async function POST(request: NextRequest) {
-  const api = new ApiV1Wrapper("ユーザ登録")
-  // ログインのタイミングに必ず実行される
-  // ケース１：ユーザ登録
-  // ケース２：ユーザ存在確認
-
-  return await api.execute("RegisterUser", async () => {
-    await api.authorize(request)
-
-    return {
-      state: "register",
-      user: {
-        id: "",
-        name: "ユーザ名",
-        birthDayDate: new Date().toISOString(),
-        role: "USER",
-      },
-    }
-  })
-}
