7696470ad53581990e950f15a9d599fd010f4b76
sakho13 <wecl.khotaro@gmail.com>
2025-04-21 07:31:15 +0900

    ✨ UserRecommendExerciseSectionにCarouselコンポーネントを追加し、レイアウトを改善

--- src/components/organisms/UserRecommendExerciseSection.tsx
+++ src/components/organisms/UserRecommendExerciseSection.tsx
@@ -6,6 +6,13 @@ import { joincn } from "@/lib/functions/joincn"
 import { useRouter } from "next/navigation"
 import { useGetRecommendExercises } from "@/hooks/useApiV1"
 import { InfoArea } from "../atoms/InfoArea"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel"
 
 export function UserRecommendExerciseSection() {
   const router = useRouter()
@@ -23,29 +30,44 @@ export function UserRecommendExerciseSection() {
         </button>
       }
     >
-      <div className='flex gap-x-3'>
-        {dataTooGetRecommendExercises?.success
-          ? dataTooGetRecommendExercises.data.recommendExercises.map(
-              (exercise) => (
-                <InfoArea
-                  key={encodeBase64(exercise.id)}
-                  className={joincn(
-                    `bg-background-subtle p-4 rounded-2xl shadow w-[300px]`,
-                    `hover:shadow-lg transition`,
-                    `cursor-pointer`,
-                  )}
-                  onClick={() =>
-                    router.push(
-                      `/v1/user/exercise?eid=${encodeBase64(exercise.id)}`,
-                    )
-                  }
-                >
-                  <h3 className='text-lg font-bold mb-2'>{exercise.title}</h3>
-                  <p className='text-sm'>{exercise.description}</p>
-                </InfoArea>
-              ),
-            )
-          : null}
+      <div className='w-full'>
+        <Carousel className='w-full '>
+          <CarouselContent>
+            {dataTooGetRecommendExercises?.success
+              ? dataTooGetRecommendExercises.data.recommendExercises.map(
+                  (exercise) => (
+                    <CarouselItem
+                      key={encodeBase64(exercise.id)}
+                      className='md:basis-1/3'
+                    >
+                      <InfoArea
+                        className={joincn(
+                          `bg-background-subtle p-4 rounded-2xl shadow h-[160px]`,
+                          `hover:shadow-lg transition`,
+                          `cursor-pointer`,
+                        )}
+                        onClick={() =>
+                          router.push(
+                            `/v1/user/exercise?eid=${encodeBase64(
+                              exercise.id,
+                            )}`,
+                          )
+                        }
+                      >
+                        <h3 className='text-lg font-bold mb-2'>
+                          {exercise.title}
+                        </h3>
+                        <p className='text-sm'>{exercise.description}</p>
+                      </InfoArea>
+                    </CarouselItem>
+                  ),
+                )
+              : null}
+          </CarouselContent>
+
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </UserLayoutSection>
   )
