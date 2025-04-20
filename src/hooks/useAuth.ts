773dcbc64944c228c4188f34830014c7d21aab69
sakho13 <wecl.khotaro@gmail.com>
2025-04-20 21:12:53 +0900

    ⚡️ 冗長な GET /api/user/v1/user-config リクエストの改善

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { onIdTokenChanged } from "firebase/auth"
 import { firebaseAuthClient } from "@/lib/functions/firebaseClient"
 import { useAuthStore } from "./stores/useAuthStore"
@@ -10,29 +10,11 @@ import { useGetUserConfig } from "./useApiV1"
 export function useAuth() {
   const { setAuth, clearAuth, idToken, user } = useAuthStore.getState()
   const { clearConfig, setConfig } = useUserConfigStore.getState()
-  const { refetchUserConfig, dataTooGetUserConfig } = useGetUserConfig(idToken)
+  const { refetchUserConfig } = useGetUserConfig(idToken)
 
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    const unsubscribe = onIdTokenChanged(
-      firebaseAuthClient,
-      async (firebaseUser) => {
-        if (firebaseUser) {
-          setAuth(firebaseUser, (await firebaseUser?.getIdToken()) ?? "")
-          refetchUserConfig()
-        } else {
-          clearAuth()
-          clearConfig()
-        }
-
-        setLoading(false)
-      },
-    )
-    return () => unsubscribe()
-  }, [clearAuth, setAuth, clearConfig, refetchUserConfig])
-
-  useEffect(() => {
+  const fetchUserConfig = useCallback(() => {
     refetchUserConfig()
       .then((r) => {
         if (r?.success) {
@@ -44,7 +26,25 @@ export function useAuth() {
       .catch(() => {
         clearConfig()
       })
-  }, [clearConfig, dataTooGetUserConfig, refetchUserConfig, setConfig])
+  }, [refetchUserConfig, setConfig, clearConfig])
+
+  useEffect(() => {
+    const unsubscribe = onIdTokenChanged(
+      firebaseAuthClient,
+      async (firebaseUser) => {
+        if (firebaseUser) {
+          setAuth(firebaseUser, (await firebaseUser?.getIdToken()) ?? "")
+          fetchUserConfig()
+        } else {
+          clearAuth()
+          clearConfig()
+        }
+
+        setLoading(false)
+      },
+    )
+    return () => unsubscribe()
+  }, [clearAuth, setAuth, clearConfig, fetchUserConfig])
 
   return { user, idToken, loading }
 }
