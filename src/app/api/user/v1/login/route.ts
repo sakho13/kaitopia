2e1383f776d6ef2a18b64ee86c256d83c199513a
sakho13 <wecl.khotaro@gmail.com>
2025-04-20 14:52:09 +0900

    ✨ ユーザ登録用のPOSTエンドポイントを新規作成

--- src/app/api/user/v1/login/route.ts
+++ src/app/api/user/v1/login/route.ts
@@ -1,4 +1,6 @@
 import { ApiV1Wrapper } from "@/lib/classes/common/ApiV1Wrapper"
+import { UserService } from "@/lib/classes/services/UserService"
+import { prisma } from "@/lib/prisma"
 import { NextRequest } from "next/server"
 
 export async function POST(request: NextRequest) {
@@ -9,6 +11,8 @@ export async function POST(request: NextRequest) {
 
   return await api.execute("RegisterUser", async () => {
     await api.authorize(request)
+
+    const userService = new UserService(prisma)
 
     return {
       state: "register",
