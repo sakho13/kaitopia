0df52518dbb7ab287a55b9c5416f19d92cb0677c
sakho13 <wecl.khotaro@gmail.com>
2025-04-22 07:47:22 +0900

    🔧 Firebaseエミュレーターの接続を追加し、初期化処理を改善

--- src/lib/functions/firebaseAdmin.ts
+++ src/lib/functions/firebaseAdmin.ts
@@ -1,17 +1,31 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app"
 import { DecodedIdToken, getAuth } from "firebase-admin/auth"
 
+const useEmulator = process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "true"
+if (useEmulator) {
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = "localhost:9099"
+  process.env.GCLOUD_PROJECT = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+}
+
 if (!getApps().length) {
-  initializeApp({
-    credential: cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.NEXT_PRIVATE_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.NEXT_PRIVATE_FIREBASE_PRIVATE_KEY?.replace(
-        /\\n/g,
-        "\n",
-      ),
-    }),
-  })
+  const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+  if (useEmulator) {
+    initializeApp({
+      projectId,
+    })
+  } else {
+    const pk = process.env.NEXT_PRIVATE_FIREBASE_PRIVATE_KEY?.replace(
+      /\\n/g,
+      "\n",
+    )
+    initializeApp({
+      credential: cert({
+        projectId,
+        clientEmail: process.env.NEXT_PRIVATE_FIREBASE_CLIENT_EMAIL,
+        privateKey: pk,
+      }),
+    })
+  }
 }
 
 export async function verifyIdToken(idToken: string) {
