572ed3fe1ae01c6d55728c51ab2e3f56633026af
sakho13 <wecl.khotaro@gmail.com>
2025-04-26 18:30:30 +0900

    💄 ButtonBaseのカラーモードにdangerを追加

--- src/components/atoms/ButtonBase.tsx
+++ src/components/atoms/ButtonBase.tsx
@@ -3,7 +3,7 @@ import { joincn } from "@/lib/functions/joincn"
 type Props = {
   children: React.ReactNode
   sizeMode?: "fit" | "full"
-  colorMode?: "primary" | "smart" | "ghost" | "outline"
+  colorMode?: "primary" | "smart" | "ghost" | "outline" | "danger"
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 export function ButtonBase({
@@ -20,6 +20,7 @@ export function ButtonBase({
     smart: "bg-smart text-text-on-color hover:bg-smart-hover",
     ghost: "bg-gray-200 hover:bg-gray-300",
     outline: "border border-gray-300 hover:bg-gray-50",
+    danger: "bg-red-600 text-text-on-color hover:bg-red-700",
   }
 
   return (
