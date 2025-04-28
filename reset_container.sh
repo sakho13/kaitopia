a9a6c434af787c5bb7fb286d6e399a52611bb6a1
sakho13 <wecl.khotaro@gmail.com>
2025-04-28 22:00:33 +0900

    🔨 開発環境を整理

--- reset_container.sh
+++ reset_container.sh
@@ -3,13 +3,11 @@
 # 実行フロー
 # 1. 起動中のコンテナを停止し削除する
 # 2. ボリュームを削除
-# 3. イメージを削除
-# 4. 新しいイメージをビルド
-# 5. コンテナを起動
+# 3. 新しいイメージをビルド
+# 4. コンテナを起動
 
 # メインロジック
 
 docker-compose down
 docker volume rm $(docker volume ls -q)
-docker rmi $(docker images -q)
-docker-compose up -d
+docker-compose --env-file ./.env.local up -d
