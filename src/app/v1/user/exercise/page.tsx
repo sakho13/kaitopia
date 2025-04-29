e78f8b4705644c99f9f58e999263c8ccaaecfaeb
sakho13 <wecl.khotaro@gmail.com>
2025-04-29 14:08:49 +0900

    ✨ 問題集ページにおすすめの問題を表示し、詳細ダイアログを追加

--- src/app/v1/user/exercise/page.tsx
+++ src/app/v1/user/exercise/page.tsx
@@ -1,53 +1,165 @@
 "use client"
 
+import { ButtonBase } from "@/components/atoms/ButtonBase"
 import { InfoArea } from "@/components/atoms/InfoArea"
 import { SectionTitle } from "@/components/atoms/SectionTitle"
-import { useGetRecommendExercises } from "@/hooks/useApiV1"
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog"
+import { useGetRecommendExercises, useGetUserExercise } from "@/hooks/useApiV1"
+import { decodeBase64 } from "@/lib/functions/decodeBase64"
 import { encodeBase64 } from "@/lib/functions/encodeBase64"
 import { joincn } from "@/lib/functions/joincn"
-import { useParams, useRouter } from "next/navigation"
-import { useEffect } from "react"
+import { DialogTitle } from "@radix-ui/react-dialog"
+import { Undo } from "lucide-react"
+import { useSearchParams, useRouter } from "next/navigation"
+import { useEffect, useMemo, useState } from "react"
 
 export default function Page() {
-  const router = useRouter()
-  const param = useParams<{ eid?: string }>()
-
-  const { dataTooGetRecommendExercises } = useGetRecommendExercises()
-
-  useEffect(() => {
-    //
-  }, [param.eid])
+  const {
+    dataToGetUserExercise,
+    dataTooGetRecommendExercises,
+    onBack,
+    startExercise,
+    openExerciseInfoDialog,
+    onOpenExerciseInfoDialog,
+    onCloseExerciseInfoDialog,
+  } = usePage()
 
   return (
     <div className='p-6 max-w-6xl mx-auto'>
-      <SectionTitle title='おすすめの問題集' />
+      <div className='mb-6'>
+        <ButtonBase colorMode='ghost' className='px-4' onClick={onBack}>
+          <Undo />
+          戻る
+        </ButtonBase>
+      </div>
 
-      <section className='mb-6 flex gap-x-3'>
-        {dataTooGetRecommendExercises && dataTooGetRecommendExercises.success
-          ? dataTooGetRecommendExercises.data.recommendExercises.map(
-              (exercise) => (
-                <InfoArea
-                  key={encodeBase64(exercise.id)}
-                  className={joincn(
-                    `bg-background-subtle p-4 rounded-2xl shadow w-[300px]`,
-                    `hover:shadow-lg transition`,
-                    `cursor-pointer`,
-                  )}
-                  onClick={() =>
-                    router.push(
-                      `/v1/user/exercise/venue?eid=${encodeBase64(
-                        exercise.id,
-                      )}`,
-                    )
-                  }
-                >
-                  <h3 className='text-lg font-bold mb-2'>{exercise.title}</h3>
-                  <p className='text-sm'>{exercise.description}</p>
-                </InfoArea>
-              ),
-            )
-          : null}
-      </section>
+      <div>
+        <SectionTitle title='おすすめの問題集' />
+
+        <section className='mb-6 flex gap-x-3'>
+          {dataTooGetRecommendExercises && dataTooGetRecommendExercises.success
+            ? dataTooGetRecommendExercises.data.recommendExercises.map(
+                (exercise) => (
+                  <InfoArea
+                    key={encodeBase64(exercise.id)}
+                    className={joincn(
+                      `bg-background-subtle p-4 rounded-2xl shadow h-[160px]`,
+                      `hover:shadow-lg transition`,
+                      `cursor-pointer`,
+                    )}
+                    onClick={() => {
+                      onOpenExerciseInfoDialog(exercise.id)
+                    }}
+                  >
+                    <h3 className='text-lg font-bold mb-2'>{exercise.title}</h3>
+                    <p className='text-sm'>{exercise.description}</p>
+                  </InfoArea>
+                ),
+              )
+            : null}
+        </section>
+      </div>
+
+      <Dialog
+        open={openExerciseInfoDialog}
+        onOpenChange={onCloseExerciseInfoDialog}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              {dataToGetUserExercise?.success
+                ? dataToGetUserExercise.data.exercise.title
+                : "-"}
+            </DialogTitle>
+          </DialogHeader>
+
+          <div>
+            <section>
+              <h2>説明</h2>
+
+              <p>
+                {dataToGetUserExercise?.success
+                  ? dataToGetUserExercise.data.exercise.description
+                  : "-"}
+              </p>
+            </section>
+
+            <section>
+              <h2></h2>
+            </section>
+
+            <search className='pt-4'>
+              <ButtonBase
+                colorMode='primary'
+                sizeMode='full'
+                className='py-4 font-bold'
+                onClick={startExercise}
+              >
+                スタート！
+              </ButtonBase>
+            </search>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   )
+}
+
+function usePage() {
+  const searchParam = useSearchParams()
+  const router = useRouter()
+
+  const [exerciseId, setExerciseId] = useState<string | null>(null)
+
+  const { dataTooGetRecommendExercises } = useGetRecommendExercises()
+  const { dataToGetUserExercise } = useGetUserExercise(exerciseId || "")
+
+  useEffect(() => {
+    const eid = searchParam.get("eid")
+    if (eid) {
+      setExerciseId(decodeBase64(decodeURIComponent(eid)))
+    } else {
+      setExerciseId(null)
+    }
+  }, [searchParam])
+
+  const onBack = () => {
+    router.back()
+  }
+
+  const startExercise = () => {
+    router.push(
+      `/v1/user/exercise/venue?eid=${encodeURIComponent(
+        encodeBase64(exerciseId || ""),
+      )}`,
+    )
+  }
+
+  const openExerciseInfoDialog =
+    useMemo(
+      () =>
+        exerciseId !== null &&
+        dataToGetUserExercise &&
+        dataToGetUserExercise.success,
+      [exerciseId, dataToGetUserExercise],
+    ) ?? false
+
+  const onOpenExerciseInfoDialog = (exerciseId: string) => {
+    setExerciseId(exerciseId)
+  }
+
+  const onCloseExerciseInfoDialog = () => {
+    setExerciseId(null)
+    router.replace("/v1/user/exercise")
+  }
+
+  return {
+    dataToGetUserExercise,
+    dataTooGetRecommendExercises,
+    onBack,
+    startExercise,
+    openExerciseInfoDialog,
+    onOpenExerciseInfoDialog,
+    onCloseExerciseInfoDialog,
+  }
 }
