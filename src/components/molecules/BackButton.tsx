d6c733e230183994ac2b9210c7e125ce79ccdedf
sakho13 <wecl.khotaro@gmail.com>
2025-05-05 15:06:21 +0900

    🐛 BackButtonにリダイレクト先を制御できるよう修正

--- src/components/molecules/BackButton.tsx
+++ src/components/molecules/BackButton.tsx
@@ -3,17 +3,24 @@
 import { Undo } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { ButtonBase } from "../atoms/ButtonBase"
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime"
 
 type Props = {
   onBackCallback?: () => void
+  to?: (router: AppRouterInstance) => void
 }
 
-export function BackButton({ onBackCallback }: Props) {
+export function BackButton({ onBackCallback, to }: Props) {
   const router = useRouter()
 
   const onBack = () => {
     if (onBackCallback) {
       onBackCallback()
+    }
+
+    if (to) {
+      to(router)
+      return
     }
     router.back()
   }
