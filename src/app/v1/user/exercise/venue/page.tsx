891e34a6b5ab1f12dadca346f0d450e04ed19a9f
sakho13 <wecl.khotaro@gmail.com>
2025-05-06 14:23:53 +0900

    🐛 useRouterの削除とエクササイズIDがnullの場合のリダイレクトを削除

--- src/app/v1/user/exercise/venue/page.tsx
+++ src/app/v1/user/exercise/venue/page.tsx
@@ -1,13 +1,12 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { useRouter, useSearchParams } from "next/navigation"
+import { useSearchParams } from "next/navigation"
 import { ExerciseManager } from "@/components/organisms/ExerciseManager"
 import { decodeBase64 } from "@/lib/functions/decodeBase64"
 import { Skeleton } from "@/components/ui/skeleton"
 
 export default function ExerciseAnswerPage() {
-  const router = useRouter()
   const searchParam = useSearchParams()
 
   const [exerciseId, setExerciseId] = useState<string | null>(null)
@@ -18,17 +17,14 @@ export default function ExerciseAnswerPage() {
       setExerciseId(decodeBase64(decodeURIComponent(eid)))
     } else {
       setExerciseId(null)
-      router.push("/v1/user/exercise")
     }
-  }, [searchParam, router])
+  }, [searchParam])
+
+  if (exerciseId === null) return <Skeleton className='w-full h-[400px]' />
 
   return (
     <div className='min-h-screen bg-kaitopia-background text-kaitopia-text p-8 font-sans'>
-      {exerciseId === null || exerciseId === "" ? (
-        <Skeleton className='w-full h-[400px]' />
-      ) : (
-        <ExerciseManager exerciseId={exerciseId} />
-      )}
+      <ExerciseManager exerciseId={exerciseId} />
     </div>
   )
 }
