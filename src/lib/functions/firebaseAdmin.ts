8ca5f206dc7361e1754b2e8ecb28d148958d7e56
sakho13 <wecl.khotaro@gmail.com>
2025-05-10 13:12:36 +0900

    💚 FirebaseAdminクライアントを関数化

--- src/lib/functions/firebaseAdmin.ts
+++ src/lib/functions/firebaseAdmin.ts
@@ -1,37 +1,42 @@
-import { cert, getApps, initializeApp } from "firebase-admin/app"
+import { cert, getApp, getApps, initializeApp } from "firebase-admin/app"
 import { DecodedIdToken, FirebaseAuthError, getAuth } from "firebase-admin/auth"
 import { ApiV1Error } from "../classes/common/ApiV1Error"
 
-const useEmulator = process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "true"
-if (useEmulator) {
-  process.env.FIREBASE_AUTH_EMULATOR_HOST = "localhost:9099"
-  process.env.GCLOUD_PROJECT = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+function getFirebaseApp() {
+  const useEmulator = process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "true"
+  if (useEmulator) {
+    process.env.FIREBASE_AUTH_EMULATOR_HOST = "localhost:9099"
+    process.env.GCLOUD_PROJECT = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+  }
+
+  if (!getApps().length) {
+    const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+    if (useEmulator) {
+      initializeApp({
+        projectId,
+      })
+    } else {
+      const pk = process.env.NEXT_PRIVATE_FIREBASE_PRIVATE_KEY?.replace(
+        /\\n/g,
+        "\n",
+      )
+      initializeApp({
+        credential: cert({
+          projectId,
+          clientEmail: process.env.NEXT_PRIVATE_FIREBASE_CLIENT_EMAIL,
+          privateKey: pk,
+        }),
+      })
+    }
+  }
+  return getApp()
 }
 
-if (!getApps().length) {
-  const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-  if (useEmulator) {
-    initializeApp({
-      projectId,
-    })
-  } else {
-    const pk = process.env.NEXT_PRIVATE_FIREBASE_PRIVATE_KEY?.replace(
-      /\\n/g,
-      "\n",
-    )
-    initializeApp({
-      credential: cert({
-        projectId,
-        clientEmail: process.env.NEXT_PRIVATE_FIREBASE_CLIENT_EMAIL,
-        privateKey: pk,
-      }),
-    })
-  }
-}
+const firebaseAuth = () => getAuth(getFirebaseApp())
 
 export async function verifyIdToken(idToken: string) {
   try {
-    return await getAuth().verifyIdToken(idToken)
+    return await firebaseAuth().verifyIdToken(idToken)
   } catch (error) {
     if (error instanceof FirebaseAuthError) {
       if (error.code === "auth/id-token-expired")
