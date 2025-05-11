1558c0b72e5b393114dc3005a540d81ecccea323
sakho13 <wecl.khotaro@gmail.com>
2025-05-11 17:48:56 +0900

    💄 UserLatestResultLogsコンポーネントの表示を改善

--- src/components/organisms/UserLatestResultLogs.tsx
+++ src/components/organisms/UserLatestResultLogs.tsx
@@ -5,6 +5,7 @@ import { DateUtility } from "@/lib/classes/common/DateUtility"
 import { encodeBase64 } from "@/lib/functions/encodeBase64"
 import { joincn } from "@/lib/functions/joincn"
 import { InfoArea } from "../atoms/InfoArea"
+import { ThinMultiProgressBar } from "../atoms/ThinMultiProgressBar"
 
 export function UserLatestResultLogs() {
   const { dataToGetUserResultLogs, totalCountToGetUserResultLogs } =
@@ -22,29 +23,42 @@ export function UserLatestResultLogs() {
 
   return (
     <InfoArea colorMode='white'>
-      {dataToGetUserResultLogs.map((l) => {
+      {dataToGetUserResultLogs.map((l, i) => {
         const createdAtt = DateUtility.convertToLocalDate(l.createdAt)
         const isInProgress = l.isInProgress
 
         return (
           <div
             key={encodeBase64(l.answerLogSheetId)}
-            className={joincn("select-none", "flex gap-4")}
+            className={joincn("select-none", i === 0 ? "" : "mt-4")}
           >
-            <div>
-              <span className='text-gray-500'>
-                {DateUtility.formatDateTime(createdAtt)}
-              </span>
+            <div className='flex gap-4'>
+              <div>
+                <span className='text-gray-500'>
+                  {DateUtility.formatDateTime(createdAtt)}
+                </span>
+              </div>
+
+              <div>
+                <span className='text-text'>
+                  {l.exercise ? `問題集「${l.exercise.title}」` : ""}
+                </span>
+              </div>
+              <div>
+                {l.totalCorrectCount} / {l.totalQuestionCount}問
+                {isInProgress && " 🏃"}
+              </div>
             </div>
 
-            <div>
-              <span className='text-text'>
-                {l.exercise ? `問題集「${l.exercise.title}」` : ""}
-              </span>
-            </div>
-            <div>
-              {l.totalCorrectCount} / {l.totalQuestionCount}問
-              {isInProgress && " 🏃"}
+            <div className='px-2'>
+              <ThinMultiProgressBar
+                totalProgress={l.totalQuestionCount}
+                progresses={[
+                  { progress: l.totalUnansweredCount, color: "#d3d3d3" },
+                  { progress: l.totalCorrectCount, color: "#98d0ff" },
+                  { progress: l.totalIncorrectCount, color: "#ff6b6b" },
+                ]}
+              />
             </div>
           </div>
         )
