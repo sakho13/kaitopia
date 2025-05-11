4b443d12f565f050fd7096c1f45f695e8279ec6c
sakho13 <wecl.khotaro@gmail.com>
2025-05-11 21:18:02 +0900

    ✨ ログイン画面へのリダイレクト機能を追加し、未認証状態のメッセージを表示

--- src/app/v1/layout.tsx
+++ src/app/v1/layout.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { redirect } from "next/navigation"
+import { ButtonBase } from "@/components/atoms/ButtonBase"
 import { useAuth } from "@/hooks/useAuth"
 
 type Props = {
@@ -8,6 +10,23 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   const { loading, idToken } = useAuth()
+
+  if (!loading && idToken === null) {
+    return (
+      <div className='flex justify-center items-center flex-col gap-y-8 h-screen'>
+        <ButtonBase
+          colorMode='primary'
+          onClick={() => redirect("/public/login")}
+        >
+          ログイン画面へ
+        </ButtonBase>
+
+        <p className='text-gray-500 font-medium select-none'>
+          認証に時間がかかっているか、未認証状態です。
+        </p>
+      </div>
+    )
+  }
 
   if (loading || idToken === null) {
     return (
