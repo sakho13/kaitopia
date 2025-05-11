a7208207a56c70f57a0696b9f376103df797cb7a
sakho13 <wecl.khotaro@gmail.com>
2025-05-11 21:06:10 +0900

    ✨ base64エンコードおよびデコード用のURL対応関数を追加

--- src/lib/functions/encodeBase64.ts
+++ src/lib/functions/encodeBase64.ts
@@ -2,3 +2,7 @@ export function encodeBase64(input: string): string {
   const buffer = Buffer.from(input, "utf-8")
   return buffer.toString("base64")
 }
+
+export function encodeBase64ForUrl(input: string): string {
+  return encodeURIComponent(encodeBase64(input))
+}
