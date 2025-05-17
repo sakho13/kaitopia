8af7b2ba39bd339f7cca5d3958d560460307cf0d
sakho13 <wecl.khotaro@gmail.com>
2025-05-17 23:27:37 +0900

    🔐 環境変数を使用してアプリのタイトル、バージョン、オーナーを設定

--- src/lib/statics.ts
+++ src/lib/statics.ts
@@ -1,7 +1,7 @@
 export const STATICS = {
-  APP_TITLE: "Kaitopia",
-  APP_VERSION: "alpha 0.0.1",
-  APP_OWNER: "SaKho",
+  APP_TITLE: process.env.NEXT_PUBLIC_APP_TITLE || "title",
+  APP_VERSION: process.env.NEXT_PUBLIC_APP_VERSION || "version",
+  APP_OWNER: process.env.NEXT_PUBLIC_APP_OWNER || "owner",
 
   GOOGLE_CONTACT_FORM_URL: "https://forms.gle/smmkpkBdjGYXiZim7",
 }
