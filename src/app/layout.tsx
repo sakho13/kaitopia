30d92b3cb748de0eb70233fd6a8ab722e9b319df
sakho13 <wecl.khotaro@gmail.com>
2025-05-17 23:37:06 +0900

    📈 Firebase Analyticsコンポーネントを追加し、ページビューイベントを送信する機能を実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { ToastContainer, Slide } from "react-toastify"
+import { FirebaseAnalytics } from "@/components/molecules/FirebaseAnalytics"
 import "./globals.css"
 
 const geistSans = Geist({
@@ -28,6 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-mono`}
       >
+        <FirebaseAnalytics />
+
         {children}
 
         <ToastContainer
