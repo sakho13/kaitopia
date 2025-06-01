18e515b1030a305d9455a2d442ee3aa22f5325de
sakho13 <wecl.khotaro@gmail.com>
2025-06-01 12:57:06 +0900

    ✨ ManageQuestionRepository を追加

---
+++ src/lib/classes/repositories/ManageQuestionRepository.ts
@@ -0,0 +1,144 @@
+import {
+  QuestionAnswerTypeType,
+  QuestionTypeType,
+} from "@/lib/types/base/questionTypes"
+import { RepositoryBase } from "../common/RepositoryBase"
+import { DateUtility } from "../common/DateUtility"
+
+export class ManageQuestionRepository extends RepositoryBase {
+  public async getQuestionDetails(questionId: string) {
+    return await this.dbConnection.question.findUnique({
+      select: {
+        schoolId: true,
+        title: true,
+        createdAt: true,
+        updatedAt: true,
+        deletedAt: true,
+
+        questionType: true,
+        answerType: true,
+        isPublished: true,
+        currentVersionId: true,
+        currentVersion: true,
+        draftVersionId: true,
+        draftVersion: true,
+
+        versions: {
+          select: {
+            version: true,
+            content: true,
+            hint: true,
+
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+      },
+      where: {
+        id: questionId,
+      },
+    })
+  }
+
+  public async getQuestionVersion(questionId: string, version: number) {
+    return await this.dbConnection.questionVersion.findFirst({
+      select: {
+        content: true,
+        hint: true,
+        createdAt: true,
+        updatedAt: true,
+        question: {
+          select: {
+            schoolId: true,
+          },
+        },
+      },
+      where: {
+        questionId: questionId,
+        version: version,
+      },
+    })
+  }
+
+  public async createQuestion(
+    schoolId: string,
+    title: string,
+    questionType: QuestionTypeType,
+    answerType: QuestionAnswerTypeType,
+  ) {
+    return await this.dbConnection.question.create({
+      data: {
+        schoolId,
+        title: title,
+        questionType: questionType,
+        answerType: answerType,
+      },
+    })
+  }
+
+  public async deleteQuestion(questionId: string) {
+    return await this.dbConnection.question.update({
+      data: {
+        deletedAt: DateUtility.getNowDate(),
+      },
+      where: {
+        id: questionId,
+      },
+    })
+  }
+
+  public async createNewVersion(
+    questionId: string,
+    version: number,
+    data: {
+      content: string
+      hint?: string
+    },
+  ) {
+    return await this.dbConnection.questionVersion.create({
+      data: {
+        questionId: questionId,
+        version: version,
+        content: data.content,
+        hint: data.hint,
+      },
+    })
+  }
+
+  public async changeDraftVersion(questionId: string, version: number) {
+    return await this.dbConnection.question.update({
+      data: {
+        draftVersionId: version,
+      },
+      where: {
+        id: questionId,
+      },
+    })
+  }
+
+  public async changeCurrentVersion(questionId: string, version: number) {
+    return await this.dbConnection.question.update({
+      data: {
+        currentVersionId: version,
+      },
+      where: {
+        id: questionId,
+      },
+    })
+  }
+
+  /**
+   * 問題集と問題を関連付ける
+   */
+  public async relateQuestionToExercise(
+    questionId: string,
+    exerciseId: string,
+  ) {
+    return await this.dbConnection.exerciseQuestion.create({
+      data: {
+        questionId: questionId,
+        exerciseId: exerciseId,
+      },
+    })
+  }
+}
