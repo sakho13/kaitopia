e9ba38fcc7d0ee33ed7954a64f608304dc3cde49
sakho13 <wecl.khotaro@gmail.com>
2025-06-28 15:55:31 +0900

    ✨ PillLabelコンポーネントに新しい色モードを追加

--- src/components/atoms/PillLabel.tsx
+++ src/components/atoms/PillLabel.tsx
@@ -11,6 +11,41 @@ const ColorMap = {
     text: "text-gray-600",
     ring: "ring-gray-500/10",
   },
+  red: {
+    background: "bg-red-50",
+    text: "text-red-700",
+    ring: "ring-red-600/10",
+  },
+  yellow: {
+    background: "bg-yellow-50",
+    text: "text-yellow-800",
+    ring: "ring-yellow-600/20",
+  },
+  green: {
+    background: "bg-green-50",
+    text: "text-green-700",
+    ring: "ring-green-600/20",
+  },
+  blue: {
+    background: "bg-blue-50",
+    text: "text-blue-700",
+    ring: "ring-blue-700/10",
+  },
+  indigo: {
+    background: "bg-indigo-50",
+    text: "text-indigo-700",
+    ring: "ring-indigo-700/10",
+  },
+  purple: {
+    background: "bg-purple-50",
+    text: "text-purple-700",
+    ring: "ring-purple-700/10",
+  },
+  pink: {
+    background: "bg-pink-50",
+    text: "text-pink-700",
+    ring: "ring-pink-700/10",
+  },
 }
 
 export function PillLabel({ label, colorMode }: Props) {
