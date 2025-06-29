f179e3f4f0b5a9d155ddc33019f8cee50edf9cd0
sakho13 <wecl.khotaro@gmail.com>
2025-06-29 17:02:59 +0900

    💄 KaTex用のスタイルシートとスクリプトを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ja'>
+      <head>
+        <link
+          rel='stylesheet'
+          href='https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.css'
+          integrity='sha384-o3WH+1yEhq+grOgz1BVYTZPyTlMXrDxnjN1By9/ba94JqJhva6wFm2Hb+URQX53v'
+          crossOrigin='anonymous'
+        />
+        <script
+          defer
+          src='https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.js'
+          integrity='sha384-C5yZTsgLOfuizO9kb+hrB8uSBwwvZ4yenKWU0KmWl+7bkL6Tph/KbcOa3S4zdoRE'
+          crossOrigin='anonymous'
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-mono`}
       >
