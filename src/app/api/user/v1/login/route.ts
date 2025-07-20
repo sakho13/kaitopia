dc586fea9aff718df5108f40a9cfae8dfab378e8
sakho13 <wecl.khotaro@gmail.com>
2025-07-20 18:56:33 +0900

    ♻️ ログインAPIをUserService2へ移行

--- src/app/api/user/v1/login/route.ts
+++ src/app/api/user/v1/login/route.ts
@@ -1,68 +1,88 @@
-import { ApiV1Wrapper } from "@/lib/classes/common/ApiV1Wrapper"
-import { UserService } from "@/lib/classes/services/UserService"
-import { prisma } from "@/lib/prisma"
 import { NextRequest } from "next/server"
+import { prisma } from "@/lib/prisma"
+import { ApiV1Wrapper } from "@/lib/classes/common/ApiV1Wrapper"
+import { UserEntity } from "@/lib/classes/entities/UserEntity"
+import { PrismaSchoolRepository } from "@/lib/classes/repositories/PrismaSchoolRepository"
+import { PrismaUserRepository } from "@/lib/classes/repositories/PrismaUserRepository"
+import { UserService2 } from "@/lib/classes/services/UserService2"
+import { DateUtility } from "@/lib/classes/common/DateUtility"
 
 export async function POST(request: NextRequest) {
   const api = new ApiV1Wrapper("ユーザ登録")
   // ログインのタイミングに必ず実行される
   // ケース１：ユーザ登録
   // ケース２：ユーザ存在確認
+  // ケース３：再ログイン
 
   return await api.execute("PostUserLogin", async () => {
     const { email, phoneNumber } = await api.authorize(request)
 
-    const userService = new UserService(prisma)
+    const userService = new UserService2(
+      prisma,
+      new PrismaUserRepository(prisma),
+      new PrismaSchoolRepository(prisma),
+    )
 
-    const userInfo = await userService.getUserInfo(api.getFirebaseUid())
-    const isGuest = await api.isGuest()
+    const user = await userService.getUserInfo(api.getFirebaseUid())
 
-    if (userInfo) {
+    if (user && !user.isDeleted) {
       return {
         state: "login",
         user: {
-          id: userInfo.id,
-          name: userInfo.name,
-          email: userInfo.email,
-          phoneNumber: userInfo.phoneNumber,
-          birthDayDate: userInfo.birthDayDate
-            ? userInfo.birthDayDate.toISOString()
+          id: user.userId,
+          name: user.value.name,
+          email: user.value.email,
+          phoneNumber: user.value.phoneNumber,
+          birthDayDate: user.value.birthDayDate
+            ? user.value.birthDayDate.toISOString()
             : null,
-          role: userInfo.role,
+          role: user.value.role,
         },
-        isGuest,
+        isGuest: user.isGuest,
       }
+    }
+
+    if (user && user.isDeleted) {
+      // ユーザが削除されている場合の処理
     }
 
     // ユーザ登録
 
     // ユーザ名はランダムで生成する(今後、ログイン時に登録するようにする)
     const userName = `user-${Math.floor(Math.random() * 10000)}`
+    const isGuest = await api.isGuest()
 
-    const user = await userService.registerUserInfo(
-      api.getFirebaseUid(),
-      isGuest,
-      {
+    const newUser = await userService.registerUserInfo(
+      new UserEntity({
+        firebaseUid: api.getFirebaseUid(),
+        id: "", // IDは自動生成されるため空文字
         name: userName,
-        role: "USER",
         email: email ?? null,
         phoneNumber: phoneNumber ?? null,
-      },
+        role: "USER",
+        birthDayDate: null, // 初期値はnull
+        createdAt: DateUtility.getNowDate(),
+        updatedAt: DateUtility.getNowDate(),
+        deletedAt: null,
+        isGuest,
+        memberSchools: [],
+        ownerSchools: [],
+      }),
     )
 
     return {
       state: "register",
       user: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        phoneNumber: user.phoneNumber,
-        birthDayDate: user.birthDayDate
-          ? user.birthDayDate.toISOString()
+        id: newUser.userId,
+        name: newUser.value.name,
+        email: newUser.value.email,
+        phoneNumber: newUser.value.phoneNumber,
+        birthDayDate: newUser.value.birthDayDate
+          ? newUser.value.birthDayDate.toISOString()
           : null,
-        role: user.role,
+        role: newUser.value.role,
       },
-      isGuest,
+      isGuest: newUser.isGuest,
     }
   })
 }
--- src/lib/classes/services/UserService2.ts
+++ src/lib/classes/services/UserService2.ts
@@ -20,30 +20,12 @@ export class UserService2 {
     return await this._userRepository.findByFirebaseUid(firebaseUid)
   }
 
-  public async registerUserInfo(
-    firebaseUid: string,
-    isGuest: boolean,
-    data: UserBaseInfo,
-  ) {
+  public async registerUserInfo(userEntity: UserEntity) {
     return this._dbConnection.$transaction(async (t) => {
       const userRepository = new PrismaUserRepository(t)
       const schoolRepository = new PrismaSchoolRepository(t)
-      const newUser = new UserEntity({
-        firebaseUid,
-        id: "", // IDは自動生成されるため空文字
-        name: data.name,
-        email: data.email,
-        phoneNumber: data.phoneNumber,
-        role: data.role,
-        birthDayDate: null, // 初期値はnull
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        deletedAt: null,
-        isGuest,
-        memberSchools: [],
-        ownerSchools: [],
-      })
-      const user = await userRepository.create(newUser)
+
+      const user = await userRepository.create(userEntity)
       await schoolRepository.createSelfSchool(user)
       return user
     })
