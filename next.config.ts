b3cd3e585ced4c2aa6563fbda1ad5c8a83193988
sakho13 <wecl.khotaro@gmail.com>
2025-07-21 09:04:32 +0900

    ✨ CORSヘッダーをnext.config.tsに追加

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,28 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_APP_TITLE: "Kaitopia",
     NEXT_PUBLIC_APP_OWNER: "SaKho",
   },
+  headers: async () => [
+    {
+      source: "/api/:path*",
+      headers: [
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET, POST, PUT, DELETE, OPTIONS",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value: "Content-Type, Authorization",
+        },
+        {
+          key: "Access-Control-Allow-Origin",
+          value:
+            process.env.NODE_ENV === "development"
+              ? "http://localhost:3000"
+              : "https://kaitopia.net",
+        },
+      ],
+    },
+  ],
 }
 
 export default nextConfig
